c563a4bc3369fcbdef6f78e73708c623f1ba2b46
江代为 <2638502607@qq.com>
2022-09-01 21:22:59 +0800

    项目第二天

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,29 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("苹果");
+//        brandService.save(brandEntity);
+
+//        brandService.removeById(1);
+
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(2L);
+        brandEntity.setName("vivo");
+        brandService.updateById(brandEntity);
     }
 
 }
